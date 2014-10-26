c4b7e0202523f5a4fd539619cd0d7d26d67d1603
Aria Stewart <aredridel@nbtsc.org>
2014-10-26 00:04:57 -0400

    My god, express session stores are gross

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var kraken = require('kraken-js');
 var express = require('express');
 var app = express();
+var db = require('./lib/db');
 
-app.use(kraken());
+app.use(kraken({
+    protocols: {
+        'leveldb-init-random': require('shortstop-leveldb-init-random')(db)
+    }
+}));
 
 app.listen(process.env.PORT || 7772);
