0fc8834f531a747abd4e17b894b4b5885bcb7919
Aria Stewart <aredridel@dinhe.net>
2015-11-04 20:24:42 -0700

    Radically simplify random

--- controllers/random.js
+++ controllers/random.js
@@ -4,16 +4,11 @@ var concat = require('concat-stream');
 module.exports = function (router) {
     router.get('/', function (req, res) {
         db.sublevel('sites').createReadStream().pipe(concat(function (sites) {
+            sites = sites.filter(function (e) { return e.key != 'HEAD' });
+
             var site = sites[Math.floor(Math.random() * sites.length)];
 
-            db.sublevel('sites').get(site.value, function (err, data) {
-                if (!err && data) {
-                    res.redirect(data);
-                } else {
-                    res.send(500, err.toString());
-                }
-            });
-        }
-        ));
+            res.redirect(site.key);
+        }));
     });
 };
